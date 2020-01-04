6fb20c4059be5bb40e363ed87bb06fe7ed2656f5
xiyichen <50854238+xiyichen@users.noreply.github.com>
2020-01-04 11:33:23 +0800

    code update

--- cleanup.py
+++ cleanup.py
@@ -3,12 +3,14 @@ import pandas as pd
 if __name__ == '__main__':
     df_deep_features = pd.read_csv('soccer-dataset/soccer_deep_features.csv')
     df_ultraface = pd.read_csv('soccer-dataset/soccer_ultraface.csv')
+    # get identity and file numbers for each face
     df_ultraface['identity'] = df_ultraface['FILE'].str.split('/').str[5].astype(int)
     df_ultraface['file'] = df_ultraface['FILE'].str.split('/').str[6]
     df_ultraface['file'] = df_ultraface['file'].str[:-4].astype(int)
     df_deep_features['identity'] = df_deep_features['FILE'].str.split('/').str[5].astype(int)
     df_deep_features['file'] = df_deep_features['FILE'].str.split('/').str[6]
     df_deep_features['file'] = df_deep_features['file'].str[:-4].astype(int)
+    # remove the wrong images
     df_ultraface = df_ultraface[~((df_ultraface['identity'] == 1) & (df_ultraface['file'] > 76))]
     df_ultraface = df_ultraface[~((df_ultraface['identity'] == 2) & (df_ultraface['file'] > 85))]
     df_ultraface = df_ultraface[~((df_ultraface['identity'] == 6) & (df_ultraface['file'] > 76))]
@@ -39,6 +41,8 @@ if __name__ == '__main__':
     df_deep_features = df_deep_features[~((df_deep_features['identity'] == 41) & (df_deep_features['file'] > 75))]
     df_deep_features = df_deep_features[~((df_deep_features['identity'] == 45) & (df_deep_features['file'] > 80))]
     df_deep_features = df_deep_features[~((df_deep_features['identity'] == 47) & (df_deep_features['file'] > 75))]
+    df_ultraface.drop(['FILE'], axis=1, inplace=True)
+    df_deep_features.drop(['FILE'], axis=1, inplace=True)
     df_ultraface.reset_index(drop=True, inplace=True)
     df_deep_features.reset_index(drop=True, inplace=True)
     df_ultraface.to_csv('soccer-dataset/soccer_ultraface_cleanup.csv')
--- gallery.py
+++ gallery.py
@@ -14,37 +14,44 @@ def build_gallery(df_deep_features, df_ultraface): # choose 2 perfect images for
     gallery_deep_features = pd.DataFrame(columns=df_deep_features.columns[:-1])
     gallery_ultraface = pd.DataFrame()
     for id, df_id in identity_group:
+        # include 40 identities (80% of the dataset) in the gallery
         if id < 40 and id != 2 and id != 33 and id != 34:
-            # sort ascendingly by the absolute value of YAW, ROLL, PITCH
+            # drop files with more than one faces to ensure the accuracy of choosing the correct identity
             df_id = df_id.drop_duplicates(subset=['file'], keep=False)
+            # get absolute values for YAW, ROLL, PITCH
             df_id['A'] = df_id['YAW'].abs()
             df_id['B'] = df_id['ROLL'].abs()
             df_id['C'] = df_id['PITCH'].abs()
+            # sort by the sum of the absolute values
             df_id['D'] = df_id['A'] + df_id['B'] + df_id['C']
             df_id.sort_values(['D'], ascending=True, inplace=True)
             df_id.drop(['A', 'B', 'C', 'D'], axis=1, inplace=True)
+            # select the smallest two sums as the "perfect image" to put into the gallery
             perfect_images_ultraface = df_id.head(2)
-            perfect_images_deep_features = df_deep_features.loc[perfect_images_ultraface.index].drop(['file'], axis=1)
-            perfect_images_deep_features = np.mean(perfect_images_deep_features, axis=0)
             gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
+            # get the deep features of these two faces
+            perfect_images_deep_features = df_deep_features.loc[perfect_images_ultraface.index].drop(['file'], axis=1)
+            # compute the average and put into the gallery
+            perfect_images_deep_features = np.mean(perfect_images_deep_features, axis=0)
             gallery_deep_features = gallery_deep_features.append(perfect_images_deep_features, ignore_index=True)
+        # for the following three identities, the method above was not able to give the correct gallery faces, so I selected manually.
         if id == 2:
             perfect_images_ultraface = df_id[(df_id['file'] == 43) | (df_id['file'] == 53)]
+            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             perfect_images_deep_features = df_deep_features.loc[perfect_images_ultraface.index].drop(['file'], axis=1)
             perfect_images_deep_features = np.mean(perfect_images_deep_features, axis=0)
-            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             gallery_deep_features = gallery_deep_features.append(perfect_images_deep_features, ignore_index=True)
         if id == 33:
             perfect_images_ultraface = df_id[(df_id['file'] == 4) | (df_id['file'] == 54)]
+            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             perfect_images_deep_features = df_deep_features.loc[perfect_images_ultraface.index].drop(['file'], axis=1)
             perfect_images_deep_features = np.mean(perfect_images_deep_features, axis=0)
-            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             gallery_deep_features = gallery_deep_features.append(perfect_images_deep_features, ignore_index=True)
         if id == 34:
             perfect_images_ultraface = df_id[(df_id['file'] == 22) | (df_id['file'] == 54)]
+            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             perfect_images_deep_features = df_deep_features.loc[perfect_images_ultraface.index].drop(['file'], axis=1)
             perfect_images_deep_features = np.mean(perfect_images_deep_features, axis=0)
-            gallery_ultraface = gallery_ultraface.append(perfect_images_ultraface)
             gallery_deep_features = gallery_deep_features.append(perfect_images_deep_features, ignore_index=True)
     gallery_ultraface.reset_index(drop=True, inplace=True)
     gallery_deep_features['identity'].astype(int)
